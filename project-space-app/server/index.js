28de9b8b8dca79aa68cfbda14d5953ac7f0a3d88
Li Haiyun <li_haiyun@nyp.edu.sg>
2025-01-13 11:35:06 +0800

    Use import

--- project-space-app/server/index.js
+++ project-space-app/server/index.js
@@ -1,21 +1,27 @@
-const express = require('express');
-const cors = require('cors');
-require('dotenv').config();
+// Import packages
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
 
+// Configure dotenv to load environment variables
+dotenv.config();
+
+// Create an Express application
 const app = express();
+
+// Middleware
+app.use(cors(
+    { origin: process.env.CLIENT_URL }
+));
 app.use(express.json());
 
-// Enable CORS
-app.use(cors({
-    origin: process.env.CLIENT_URL
-}));
-
-// Simple Route
-app.get("/", (req, res) => {
-    res.send("Welcome to the project space.");
+// Define routes
+app.get('/', (req, res) => {
+  res.send('Welcome to the project space!');
 });
 
-let port = process.env.APP_PORT;
-app.listen(port, () => {
-    console.log(`⚡ Sever running on http://localhost:${port}`);
+// Start the server
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
