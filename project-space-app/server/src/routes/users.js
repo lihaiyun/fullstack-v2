34d79cb429d12de6f41fb0957d76f8e54cc9327d
Li Haiyun <li_haiyun@nyp.edu.sg>
2025-01-31 18:13:05 +0800

    user login

--- project-space-app/server/src/routes/users.js
+++ project-space-app/server/src/routes/users.js
@@ -1,12 +1,13 @@
 import express from "express";
 import yup from "yup";
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
 import User from "../models/user.js";
 
 const router = express.Router();
 
-const userSchema = yup.object().shape({
+const registerSchema = yup.object().shape({
     name: yup.string().trim()
         .required('Name is required')  
         .matches(/^[a-zA-Z '-,.]+$/, 'Name must only contain letters, spaces and characters: \'-,.')
@@ -17,14 +18,27 @@ const userSchema = yup.object().shape({
         .max(100, 'Email must be at most 100 characters'),
     password: yup.string().trim()
         .required('Password is required')
-        .matches(/^(?=.*[a-zA-Z])(?=.*[0-9]).{8,}$/, 'Password must be at least 8 characters long and contain at least one letter and one number')
+        .min(8, 'Password must be at least 8 characters')
+        .matches(/^(?=.*[a-zA-Z])(?=.*[0-9]).*$/, 'Password must contain at least one letter and one number')
         .max(50, 'Password must be at most 50 characters')
     });
+
+const loginSchema = yup.object().shape({
+    email: yup.string().trim()
+        .required('Email is required')
+        .email('Email must be a valid email address')
+        .max(100, 'Email must be at most 100 characters'),
+    password: yup.string().trim()
+        .required('Password is required')
+        .min(8, 'Password must be at least 8 characters')
+        .matches(/^(?=.*[a-zA-Z])(?=.*[0-9]).*$/, 'Password must contain at least one letter and one number')
+        .max(50, 'Password must be at most 50 characters')
+});
 
 router.post("/register", async (req, res) => {
     let data = req.body;
     try {
-        data = await userSchema.validate(data, { abortEarly: false });
+        data = await registerSchema.validate(data, { abortEarly: false });
         //console.log(data);
     } catch (err) {
         return res.status(400).json({ message: err.errors.join(", ") });
@@ -53,6 +67,41 @@ router.post("/register", async (req, res) => {
         console.error(err);
         res.status(500).json({ message: "Failed to register user" });
     }
+});
+
+router.post("/login", async (req, res) => {
+    let data = req.body;
+    try {
+        data = await loginSchema.validate(data, { abortEarly: false });
+        //console.log(data);
+    } catch (err) {
+        return res.status(400).json({ message: err.errors.join(", ") });
+    }
+
+    // Check if the email and password match
+    const { email, password } = data;
+    const user = await User.findOne({ email });
+    if (!user) {
+        return res.status(400).json({ message: "Invalid email or password" });
+    }
+
+    const passwordMatch = await bcrypt.compare(password, user.password);
+    if (!passwordMatch) {
+        return res.status(400).json({ message: "Invalid email or password" });
+    }
+
+    // Generate JWT token
+    const userInfo = {
+        _id: user._id,
+        name: user.name,
+        email: user.email
+    };
+    const accessToken = jwt.sign(userInfo, process.env.APP_SECRET, 
+        { expiresIn: process.env.TOKEN_EXPIRES_IN });
+    res.json({
+        accessToken,
+        user: userInfo
+    });
 });
 
 export default router;
