6cfef9281f3a714baee954450f01755019e4aee6
Li Haiyun <li_haiyun@nyp.edu.sg>
2025-01-31 16:03:49 +0800

    import { Provider } from "@/components/ui/provider"

--- project-space-app/client/src/App.jsx
+++ project-space-app/client/src/App.jsx
@@ -1,10 +1,11 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+//import "./App.css";
+import { Box, Button, Heading } from "@chakra-ui/react";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
@@ -28,8 +29,13 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+
+      <Box p={5}>
+        <Heading mb={4}>Hello, Chakra UI!</Heading>
+        <Button colorPalette="blue">Click Me</Button>
+      </Box>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- project-space-app/client/src/main.jsx
+++ project-space-app/client/src/main.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { ChakraProvider, defaultSystem } from "@chakra-ui/react"
+import { Provider } from "@/components/ui/provider"
 import App from './App.jsx'
-import './index.css'
+//import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <ChakraProvider value={defaultSystem}>
+    <Provider>
       <App />
-    </ChakraProvider>
+    </Provider>
   </React.StrictMode>,
 )
