aea90e7b70bb807efdfeb48e716f97f12bfe8c5d
Li Haiyun <li_haiyun@nyp.edu.sg>
2025-02-06 11:45:10 +0800

    Update App.jsx

--- project-space-app/client/src/App.jsx
+++ project-space-app/client/src/App.jsx
@@ -1,7 +1,5 @@
 import { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router';
-import { ThemeProvider } from '@mui/material/styles';
-import MyTheme from './themes/MyTheme';
 import Projects from './pages/Projects';
 import AddProject from './pages/AddProject';
 import EditProject from './pages/EditProject';
@@ -26,19 +24,17 @@ function App() {
   return (
     <UserContext.Provider value={{ user, setUser }}>
       <Router>
-        <ThemeProvider theme={MyTheme}>
-            <Routes>
-              <Route path={"/"} element={<Layout />}>
-                <Route index element={<Projects />} />
-                <Route path="projects" element={<Projects />} />
-                <Route path="addproject" element={<AddProject />} />
-                <Route path="editproject/:id" element={<EditProject />} />
-                <Route path="register" element={<Register />} />
-                <Route path="login" element={<Login />} />
-                <Route path="form" element={<MyForm />} />
-              </Route>
-            </Routes>
-        </ThemeProvider>
+          <Routes>
+            <Route path={"/"} element={<Layout />}>
+              <Route index element={<Projects />} />
+              <Route path="projects" element={<Projects />} />
+              <Route path="addproject" element={<AddProject />} />
+              <Route path="editproject/:id" element={<EditProject />} />
+              <Route path="register" element={<Register />} />
+              <Route path="login" element={<Login />} />
+              <Route path="form" element={<MyForm />} />
+            </Route>
+          </Routes>
       </Router>
     </UserContext.Provider>
   );
