b54cc4d4c098c4d406eb99d9ec8ee56384dde989
Li Haiyun <li_haiyun@nyp.edu.sg>
2025-06-05 21:29:55 +0800

    Update page.tsx

--- project-space-next/project-space-app/src/app/user/register/page.tsx
+++ project-space-next/project-space-app/src/app/user/register/page.tsx
@@ -4,9 +4,18 @@ export default async function Register() {
             <h1 className="text-2xl font-bold mb-4">Register</h1>
             <form className="w-full max-w-md">
                 <div className="mb-4">
+                    <label className="block text-sm font-medium mb-2" htmlFor="name">Name</label>
+                    <input
+                        type="text"
+                        id="name"
+                        name="name"
+                        className="w-full px-3 py-2 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-blue-500"
+                        required
+                    />
+                </div>
+                <div className="mb-4">
                     <label className="block text-sm font-medium mb-2" htmlFor="email">Email</label>
                     <input
-                        type="email"
                         id="email"
                         name="email"
                         className="w-full px-3 py-2 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-blue-500"
